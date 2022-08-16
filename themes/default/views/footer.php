67d1807e7aec21fb0f7adcc2eab256a9d73f5a28
tempebener <bayoewa@gmail.com>
2022-08-16 15:57:20 +0700

    menu profile

--- themes/default/views/footer.php
+++ themes/default/views/footer.php
@@ -11,7 +11,7 @@
                                  Copyright &copy; <?= date('Y') . ' ' . $Settings->site_name; ?>. All rights reserved.
                             </div>
                         </div>
-                        <div class="col-lg-6">
+                        <!-- <div class="col-lg-6">
                             <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                                 <li class="nav-item">
                                     <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
@@ -26,7 +26,7 @@
                                     <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
                                 </li>
                             </ul>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </footer>
